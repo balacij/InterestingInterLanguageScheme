f639f6185e1eb50b72f9a5080b550d597ab4f38a
Jason Balaci <jasonbalaci@gmail.com>
2021-11-19 18:38:35 -0500

    I accidentally left ex2' as a copy of ex2, just changed it intentionally to show how we can still restrict terms of our language via the interpreters

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,12 +2,11 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FlexibleContexts #-}
 
--- I really wish we didn't need AllowAmbiguousTypes, FlexibleContexts, and FlexibleContexts, but it appears we do...
+-- I really wish we didn't need AllowAmbiguousTypes and FlexibleInstances, but it appears we do...
 
 module Lib
     ( someFunc
@@ -31,7 +30,7 @@ instance Show Json where
     show (JsonBool b) = show b
     show (JsonArray jss) = '[' : (intercalate "," (map show jss) ++ "]")
 
-data JsonConfig = JsonConfig -- in other words, an empty configuration
+data JsonConfig = JsonConfig -- in other words, an empty configuration, we can trivially add options to JsonConfig, but they are not important to this prototype (I think!)
 
 -- An important typeclass
 class CanGen i o cfg ctx where
@@ -56,17 +55,17 @@ instance CanGen (Sentence Json JsonConfig PlainCtx) Json JsonConfig PlainCtx whe
 
 data OtherCtx = OtherCtx
 
-instance CanGen (Sentence Json JsonConfig PlainCtx) Json JsonConfig OtherCtx where -- OtherCtx is a variant that discards strings (as Null, but renders everything else with a PlainCtx)
+instance CanGen (Sentence Json JsonConfig PlainCtx) Json JsonConfig OtherCtx where -- OtherCtx is a variant that discards S constructors (replace with Null, but renders everything else with a PlainCtx)
     gen (S s)         cfg ctx = JsonNull
     gen (SGen c)      cfg ctx = gen c cfg PlainCtx
     gen (SConcat l r) cfg ctx = JsonArray [gen l cfg ctx, gen r cfg ctx]
 
 
-ex2 :: Sentence Json JsonConfig PlainCtx
+ex2 :: Sentence Json JsonConfig PlainCtx -- Here, we are saying that the 'SGen' terms of the language should also be renderable to a PlainCtx version of Json, PlainCtx is a 'name' of sorts for this 'gen' instance.
 ex2 = SConcat (S "Hello, World!") (SGen $ AddE (IntE 1) (SubE (IntE 2) (IntE 3)))
 
-ex2' :: Sentence Json JsonConfig PlainCtx
-ex2' = SConcat (S "Hello, World!") (SGen $ AddE (IntE 1) (SubE (IntE 2) (IntE 3)))
+-- ex2' :: Sentence Json JsonConfig OtherCtx -- Here, we are saying that the terms of the language should also be renderable via 'OtherCtx', but the RHS expression is not!, thus, this code needs to be commented out, else an error!
+-- ex2' = SConcat (S "Hello, World!") (SGen $ AddE (IntE 1) (SubE (IntE 2) (IntE 3)))
 
 
 -- conv_ex2 :: CanGen (Sentence Json JsonConfig PlainCtx) Json JsonConfig PlainCtx => Json
@@ -75,25 +74,24 @@ conv_ex2 = gen ex2 JsonConfig PlainCtx
 
 -- conv_ex2' :: CanGen (Sentence Json JsonConfig PlainCtx) Json JsonConfig OtherCtx => Json
 -- conv_ex2' :: CanGen (Sentence Json JsonConfig PlainCtx) Json JsonConfig PlainCtx => Json -- TODO: Figure out why this type signature work too.
-conv_ex2' :: Json
-conv_ex2' = gen ex2' JsonConfig OtherCtx
+conv_ex2_othCtx :: Json
+conv_ex2_othCtx = gen ex2 JsonConfig OtherCtx
 
 s_ex2 :: String
 s_ex2 = show conv_ex2
 
-s_ex2' :: String
-s_ex2' = show conv_ex2'
+s_ex2_othCtx :: String
+s_ex2_othCtx = show conv_ex2_othCtx
 
 someFunc :: IO ()
 someFunc = do
     putStrLn s_ex2
-    putStrLn s_ex2'
+    putStrLn s_ex2_othCtx
 
 {-
 
 Important notes: 
     - we can have "higher level" gens that use "higher level configs" to use already written "gen"s that use "lower level configs"!!!!
         - This is a huge 'plus' for re-usability!
-    - 
 
 -}
