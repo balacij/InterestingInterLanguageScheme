f70baa68f627f52a80103f60d6bd13084d11f4d5
Jason Balaci <jasonbalaci@gmail.com>
2021-11-19 19:42:06 -0500

    Some commentary

--- src/Lib.hs
+++ src/Lib.hs
@@ -64,6 +64,8 @@ instance CanGen (Sentence Json JsonConfig PlainCtx) Json JsonConfig OtherCtx whe
 
 ex2 :: Sentence Json JsonConfig PlainCtx -- Here, we are saying that the 'SGen' terms of the language should also be renderable to a PlainCtx version of Json, PlainCtx is a 'name' of sorts for this 'gen' instance.
 ex2 = SConcat (S "Hello, World!") (SGen $ AddE (IntE 1) (SubE (IntE 2) (IntE 3)))
+-- Unfortunately, with ex2, we have to write the type signature manually, HLS cannot resolve it on it's own
+
 
 -- ex2' :: Sentence Json JsonConfig OtherCtx -- Here, we are saying that the terms of the language should also be renderable via 'OtherCtx', but the RHS expression is not!, thus, this code needs to be commented out, else an error!
 -- ex2' = SConcat (S "Hello, World!") (SGen $ AddE (IntE 1) (SubE (IntE 2) (IntE 3)))
@@ -85,9 +87,9 @@ s_ex2_othCtx :: String
 s_ex2_othCtx = show conv_ex2_othCtx
 
 {-
-
-NOTE: We can trim down allowed languages, but I intentionally complicated it, more practically, we might have a target language specifically that they all might be usable in.
-
+    NOTE: We can trim down allowed languages, but I intentionally complicated it above.
+    More practically, we might have a target language specifically that they should all
+    be convertible into.
 -}
 
 
@@ -101,7 +103,7 @@ instance CanGen Sentence' Json JsonConfig PlainCtx where
     gen (SGen' i)      cfg ctx = gen i cfg ctx
     gen (SConcat' l r) cfg ctx = JsonArray [gen l cfg ctx, gen r cfg ctx]
 
-ex3 :: Sentence'
+ex3 :: Sentence' -- HLS can resolve this type signature!
 ex3 = SConcat' (SGen' $ AddE (IntE 10) (IntE 30)) (SConcat' (S' "Hello, world!") (SGen' $ SubE (IntE 20) (IntE 30)))
 
 conv_ex3 :: Json
@@ -121,5 +123,13 @@ someFunc = do
 Important notes: 
     - we can have "higher level" gens that use "higher level configs" to use already written "gen"s that use "lower level configs"!!!!
         - This is a huge 'plus' for re-usability!
+    - We are defining terms accepted into a language as the ways in which it can be "printed"/"gen"ed while carried by the carrying language.
+    - A printer here is:
+        - an input
+        - an output
+        - a configuration
+        - a context of the printing output (e.g., through this, we can have multiple instances for a particular Input/Output, but
+            retain knowledge of the differentiation via the 'context'!)
+    - The above definition might also be my definition for #2896 - "What is printing?"
 
 -}
